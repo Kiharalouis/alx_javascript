0a8a86b93ceb755da537d23d44140c3aed59927f
Kiharalouis <kiharalouis@gmail.com>
2023-10-24 18:57:09 +0300

    103 bug

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,22 +2,20 @@
 const myObject = {
    type: 'object',
    value: 12
-  };
-  console.log(myObject);
-  
-  myObject.incr = function() {
+ };
+ console.log(myObject);
+ 
+ // Define the incr function as a property of myObject
+ myObject.incr = function() {
+   // Increment the value property of myObject
    this.value++;
-  };
-  
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  
-  
-  { type: 'object', value; 12 }
-  { type: 'object', value; 13, incr; [Function] }
-  { type: 'object', value; 14, incr; [Function] }
-  { type: 'object', value; 15, incr; [Function] }
+ };
+ 
+ // Call the incr function to increment the value property
+ myObject.incr();
+ console.log(myObject);
+ myObject.incr();
+ console.log(myObject);
+ myObject.incr();
+ console.log(myObject);
+ 
